1ff8d2b5567a4293da4b36ec6e61c630016532d8
Ashish Mishra <b.s.ashish26@gmail.com>
2022-08-04 18:37:45 +0530

    chore: configure babel and terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,28 @@
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+
+// const devMode = process.env.NODE_ENV === "development";
+
 export default {
   input: "./index.js",
   output: {
     file: "./dist/index.js",
     format: "esm",
     exports: "named",
+    sourcemap: "inline",
+    plugins: [
+      terser({
+        ecma: 2020,
+        mangle: { toplevel: true },
+        compress: {
+          module: true,
+          toplevel: true,
+          unsafe_arrows: true,
+          drop_console: true,
+          drop_debugger: true,
+        },
+        output: { quote_style: 1 },
+      }),
+    ],
   },
 };
