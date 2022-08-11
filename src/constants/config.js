8a71039902ee7632c0099e7630cb62a7842f2fef
Ashish Mishra <b.s.ashish26@gmail.com>
2022-08-11 12:42:53 +0530

    chore: add config

---
+++ src/constants/config.js
@@ -0,0 +1,31 @@
+export const config = {
+  event: {
+    classification: {
+      instant: [],
+    },
+    priorities: [
+      {
+        identifier: "realTime",
+        priority: 0,
+      },
+      {
+        identifier: "instant",
+        priority: 1,
+      },
+    ],
+  },
+  batch: {
+    // max interval time between two batches, in seconds
+    maxTimeBetweenTwoBatches: 10,
+    // max size of batch, in kb
+    maxBatchSize: 50000,
+  },
+  network: {
+    // url for clickstream backend service, URL instance
+    url: null,
+    // headers for the request, Headers instance
+    headers: null,
+    maxConnectionRetries: 30,
+    maxConnectionRetryInterval: 30,
+  },
+}
