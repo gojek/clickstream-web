782dad6a47789d1fc237f433be35515bcb274946
Ashish Mishra <b.s.ashish26@gmail.com>
2022-08-12 21:44:26 +0530

    build: add exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,36 @@
 import babel from "@rollup/plugin-babel"
 import { terser } from "rollup-plugin-terser"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
 
 export default {
   input: "src/index.js",
   output: [
     {
       name: "clickstream",
-      file: "./dist/clickstream.js",
+      file: "./dist/index.js",
       format: "esm",
       sourcemap: true,
     },
     {
       name: "clickstream",
-      file: "./dist/clickstream.min.js",
+      file: "./dist/index.min.js",
       format: "esm",
       plugins: [terser()],
     },
+    {
+      file: "./dist/index.cjs",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "./dist/index.min.cjs",
+      format: "cjs",
+      plugins: [terser()],
+    },
   ],
+  external: ["protobufjs/minimal.js"],
   plugins: [
+    nodeResolve(),
     babel({
       babelHelpers: "bundled",
     }),
