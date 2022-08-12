4a45cd75c65351225ff57f99f66a6a2e9b9be929
Ashish Mishra <b.s.ashish26@gmail.com>
2022-08-12 13:59:48 +0530

    chore: throw error if url and headers are not present

--- src/clickstream.js
+++ src/clickstream.js
@@ -3,6 +3,7 @@ import Processor from "./processor.js"
 import Scheduler from "./scheduler.js"
 import { CUSTOM_EVENT, EVENT_TYPE } from "./constants/index.js"
 import EventBus from "./event.js"
+import { defaultConfig } from "./constants/config.js"
 
 export default class Clickstream {
   #tracking
@@ -11,34 +12,42 @@ export default class Clickstream {
   #network
   #eventBus
   constructor({ event, batch, network } = {}) {
+    if (!network.url) {
+      throw new Error("Clickstream: Provide url in network config")
+    }
+
+    if (!network.headers.get("Authorization")) {
+      throw new Error(
+        "Clickstream: Provide Authorization header in network config"
+      )
+    }
+
     this.#tracking = true
     this.#eventBus = new EventBus()
     this.#processor = new Processor({
-      config: event,
+      config: Object.assign(defaultConfig.event, event),
     })
 
     this.#scheduler = new Scheduler({
-      config: batch,
+      config: Object.assign(defaultConfig.batch, batch),
       eventBus: this.#eventBus,
     })
 
     this.#network = new Network({
-      config: network,
+      config: Object.assign(defaultConfig.network, network),
     })
 
-    this.#start()
+    this.#init()
   }
 
-  #start() {
+  #init() {
     this.#listeners()
     this.#scheduler.start()
   }
 
   #listeners() {
-    console.log("attached")
     this.#eventBus.on(CUSTOM_EVENT.NEW_BATCH, (e) => {
-      const response = this.#network.send(e.detail.batch)
-      console.log(response)
+      this.#network.send(e.detail.batch)
     })
   }
 
@@ -50,12 +59,8 @@ export default class Clickstream {
     return new Promise((resolve) => {
       const { type, event } = this.#processor.process(proto)
 
-      console.log(type, event)
-
       if (type === EVENT_TYPE.INSTANT) {
         this.#scheduler.ingest(event)
-      } else {
-        // noop
       }
 
       resolve("success")
