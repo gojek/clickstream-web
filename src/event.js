c36a70fb7b16ce7f144b8b4f83d0349f444d2fc2
Ashish Mishra <b.s.ashish26@gmail.com>
2022-08-24 15:15:16 +0530

    chore: add event target

--- src/event.js
+++ src/event.js
@@ -1,21 +1,24 @@
 // @ts-check
 export default class EventBus {
+  constructor() {
+    this.eventTarget = new EventTarget()
+  }
   /**
-   *
+   * Event emitter
    * @param type name of the event
    * @param payload data to attach in event
    */
   emit(type, payload) {
     const event = new CustomEvent(type, { detail: payload })
-    document.dispatchEvent(event)
+    this.eventTarget.dispatchEvent(event)
   }
 
   /**
-   *
+   * Event listener
    * @param type name of the event
    * @param callback callback function
    */
   on(type, callback) {
-    document.addEventListener(type, callback)
+    this.eventTarget.addEventListener(type, callback)
   }
 }
