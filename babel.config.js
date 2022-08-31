f26782a99417f8c8a687aab3ae95ad3984a12511
Ashish Mishra <b.s.ashish26@gmail.com>
2022-08-31 15:29:45 +0530

    chore: add targets in @babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
+/**
+ * targets for babel preset env
+ * default browserslist(> 0.5%, last 2 versions, Firefox ESR, not dead)
+ * exclude ie 11
+ * supports maintained node versions by node foundation
+ * more info at https://github.com/browserslist/browserslist#query-composition
+ */
+const targets = "defaults, not ie 11, maintained node versions"
+
 export default {
-  presets: ["@babel/preset-env"],
-};
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets,
+        // Converts modern syntax that is broken in some browsers to the closest equivalent syntax that is not broken in those browsers
+        // more info - https://web.dev/serve-modern-code-to-modern-browsers/#enable-modern-bugfixes
+        bugfixes: true,
+      },
+    ],
+  ],
+}
