58fae9c405ed4e13f2c2c63f40148be12235e2d4
Ashish Mishra <b.s.ashish26@gmail.com>
2022-11-15 18:53:17 +0530

    test: udpate unit tests

--- jest.setup.js
+++
@@ -1,9 +0,0 @@
-/**
- * @see https://github.com/jsdom/jsdom/issues/2524#issuecomment-1118254830
- */
-Object.defineProperty(window, "setInteval", {
-  writable: true,
-  value: setInterval,
-})
-
-global.Headers = Headers
